3291aa12a9afc0fb30ca05dd1f4624eecde8595b
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-12-14 13:20:55 +0000

    create service provider

---
+++ src/TextManipulatorServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace BlackPanda\TextManipulator;
+
+use Illuminate\Support\ServiceProvider;
+
+class TextManipulatorServiceProvider extends ServiceProvider
+{
+
+    /**
+     * determine register method of service provider
+     */
+    public function register()
+    {
+
+    }
+
+}
